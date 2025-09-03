d55dfebf0d53642f8072194450811d4bd4144a6c
teng <teng@itu.dk>
2025-09-03 14:58:48 +0200

    Read files from chirp_cli_db.csv

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -1,7 +1,16 @@
-﻿if (args[0] == "say")
+﻿try
 {
-    var message = args[1];
-    var frequency = int.Parse(args[2]);
-    foreach (var i in Enumerable.Range(1, frequency))
-        Console.Write(message + " ");
+    // Open the text file using a stream reader.
+    using StreamReader reader = new("data/chirp_cli_db.csv");
+
+    // Read the stream as a string.
+    string text = reader.ReadToEnd();
+
+    // Write the text to the console.
+    Console.WriteLine(text);
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
 }
