e3eca906e92bf239b7d3c3e1099ffca3332905af
T.H. Engberg <tore0402@gmail.com>
2025-09-03 18:12:56 +0200

    commit before merge of main and read_csv

--- Chirp.CLI/obj/Debug/net9.0/Chirp.CLI.AssemblyInfo.cs
+++ Chirp.CLI/obj/Debug/net9.0/Chirp.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a868bb9166197fb13522b93bff0a76147109563c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Chirp.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
