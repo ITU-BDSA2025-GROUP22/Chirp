f392570521573e515351b04bfd892f8837863fe6
T.H. Engberg <tore0402@gmail.com>
2025-09-17 22:09:52 +0200

    added another comment

--- Chirp.CLI/src/Program/UserInterface.cs
+++ Chirp.CLI/src/Program/UserInterface.cs
@@ -9,7 +9,7 @@ public static class UserInterface
             Console.WriteLine($"{cheep.Author}: {cheep.Message} \n({cheep.Timestamp})\n");
         }
     }
-
+    //Static method for printing cheeps
     public static void PrintCheeps(IEnumerable<Program.Cheep> cheeps, int num)
     {
         int count = 0;
