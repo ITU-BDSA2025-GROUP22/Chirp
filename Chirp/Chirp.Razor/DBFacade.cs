bb5c0f07786b5e3f5729e95f9ac6e9c0c8e82a40
T.H. Engberg <tore0402@gmail.com>
2025-10-03 16:54:53 +0200

    added 'paged' variants of GetAllCheeps/GetCheepsByAuthor methods

--- Chirp/Chirp.Razor/DBFacade.cs
+++ Chirp/Chirp.Razor/DBFacade.cs
@@ -21,6 +21,37 @@ public class DBFacade
         // 3. Build connection string
         _connectionString = $"Data Source={dbPath}";
     }
+
+    public List<CheepViewModel> GetPagedCheeps(int page, int pageSize = 32)
+    {
+        var cheeps = new List<CheepViewModel>();
+        int offset = (page - 1) * pageSize;
+        var sqlQuery = @"SELECT u.username, m.text, m.pub_date 
+                     FROM ""message"" m 
+                     JOIN ""user"" u ON m.author_id = u.user_id 
+                     ORDER BY m.pub_date DESC
+                     LIMIT @pageSize OFFSET @offset";
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = sqlQuery;
+            command.Parameters.AddWithValue("@pageSize", pageSize);
+            command.Parameters.AddWithValue("@offset", offset);
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var author = reader.GetString(0);
+                var message = reader.GetString(1);
+                var timestamp = reader.GetInt64(2);
+                var cheep = new CheepViewModel(author, message, UnixTimeStampToDateTimeString(timestamp));
+                cheeps.Add(cheep);
+            }
+        }
+        return cheeps;
+    }
     
     public List<CheepViewModel> GetAllCheeps()
     {
@@ -62,6 +93,41 @@ public class DBFacade
             var command = connection.CreateCommand();
             command.CommandText = sqlQuery;
             command.Parameters.AddWithValue("@author", author);
+
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                var dbauthor = reader.GetString(0);
+                var message = reader.GetString(1);
+                var timestamp = reader.GetInt64(2);
+                var cheep = new CheepViewModel(dbauthor, message, UnixTimeStampToDateTimeString(timestamp));
+                cheeps.Add(cheep);
+            }
+        }
+        return cheeps;
+    }
+    
+    public List<CheepViewModel> GetPagedCheepsByAuthor(string author, int page, int pageSize = 32)
+    {
+        string sqlQuery = @"SELECT u.username, m.text, m.pub_date 
+                     FROM ""message"" m 
+                     JOIN ""user"" u ON m.author_id = u.user_id 
+                     WHERE u.username = @author
+                     ORDER BY m.pub_date DESC
+                     LIMIT @pageSize OFFSET @offset";
+        
+        var cheeps = new List<CheepViewModel>();
+        int offset = (page - 1) * pageSize;
+        
+        using (var connection = new SqliteConnection(_connectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+            command.CommandText = sqlQuery;
+            command.Parameters.AddWithValue("@author", author);
+            command.Parameters.AddWithValue("@pageSize", pageSize);
+            command.Parameters.AddWithValue("@offset", offset);
 
             using var reader = command.ExecuteReader();
             while (reader.Read())
