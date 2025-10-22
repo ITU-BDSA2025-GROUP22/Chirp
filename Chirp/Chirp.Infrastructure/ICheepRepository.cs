31d5da46e0f46ff4614d4d13474ed7cefd54f796
Nikolai Li <122288063+NikolaiLi@users.noreply.github.com>
2025-10-22 21:36:08 +0200

    AddCheep in CheepRepository implementation

--- Chirp/Chirp.Infrastructure/ICheepRepository.cs
+++ Chirp/Chirp.Infrastructure/ICheepRepository.cs
@@ -7,6 +7,5 @@ public interface ICheepRepository
     public List<CheepViewModel> GetCheeps(int page, int pageSize = 32);
     
     public List<CheepViewModel> GetCheepsByAuthor(string author, int page, int pageSize = 32);
-
     public void AddCheep(Cheep cheep);
 }
