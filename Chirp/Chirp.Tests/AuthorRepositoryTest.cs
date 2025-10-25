86a9285d132f488b3f2f5fe5bd9fde64079ef65f
Nikolai Li <122288063+NikolaiLi@users.noreply.github.com>
2025-10-25 23:23:06 +0200

    AuthorRepository - GetAuthorByNameTest
    - Still needs more unitTest for AuthorRepository

---
+++ Chirp/Chirp.Tests/AuthorRepositoryTest.cs
@@ -0,0 +1,43 @@
+﻿using Chirp.Infrastructure;
+using Chirp.Core;
+using Microsoft.EntityFrameworkCore; 
+using Xunit;
+using System.Linq;
+
+namespace Chirp.Tests;
+
+public class AuthorRepositoryTests
+{   
+    private readonly IAuthorRepository _repository;
+    private readonly ChirpContext _context;
+
+    public AuthorRepositoryTests()
+    {
+        var options = new DbContextOptionsBuilder<ChirpContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb_Author") 
+            .Options;
+        
+        _context = new ChirpContext(options);
+        
+        _context.Database.EnsureDeleted();
+        _context.Database.EnsureCreated();
+        
+        _context.Authors.AddRange(
+            new Author { Username = "Adrian", Password = "123", Email = "adrian@test.com" },
+            new Author { Username = "Børge", Password = "123", Email = "borge@test.com" }
+        );
+        
+        _context.SaveChanges();
+
+        _repository = new AuthorRepository(_context);
+    }
+
+    [Fact]
+    public void GetAuthorByNameTest()
+    {
+        var author = _repository.GetAuthorByName("Adrian");
+        
+        Assert.NotNull(author);
+        Assert.Equal("Adrian", author.Username);
+    }
+}
