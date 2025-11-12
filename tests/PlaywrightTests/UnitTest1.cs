3fd783c1adc100df9316b48cc8ce1eea88a1d983
T.H. Engberg <tore0402@gmail.com>
2025-11-12 15:28:25 +0100

    added playwright

---
+++ tests/PlaywrightTests/UnitTest1.cs
@@ -0,0 +1,44 @@
+namespace PlaywrightTests;
+
+/**
+    INSTALLATION MAY BE NEEDED TO RUN PLAYWRIGHT TESTS
+        - in the terminal, run:
+            bin/Debug/net8.0/playwright.ps1 install --with-deps
+        
+        - this is a PowerShell script - it might need execution permissions:
+            Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+    TESTING
+    run command: bin/Debug/net8.0/playwright.ps1 codegen https://1bdsagroup22chirp.azurewebsites.net/
+    to record End-To-End test
+*/
+
+public class EndToEndTests
+{
+    [SetUp]
+        public async Task Setup()
+        {
+            await Context.Tracing.StartAsync(new()
+            {
+                Screenshots = true,
+                Snapshots = true
+            });
+        }
+        [Test]
+        public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+        {
+            await Page.GotoAsync("https://1bdsagroup22chirp.azurewebsites.net/");
+            // Expect a title "to contain" a substring.
+            await Expect(Page).ToHaveTitleAsync(new Regex("Chirp!"));
+    
+            // create a locator
+            var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Register" });
+            // Expect an attribute "to be strictly equal" to the value.
+            await Expect(getStarted).ToHaveAttributeAsync("href", "/Identity/Account/Register");
+    
+            // Click the get started link.
+            await getStarted.ClickAsync();
+            // Expects the URL to contain intro.
+            await Expect(Page).ToHaveURLAsync(new Regex(".*Register"));
+        }
+}
