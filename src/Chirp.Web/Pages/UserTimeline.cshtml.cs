86c67d81c29f84608e399ed097b742e5a0630985
Nikolai Li <122288063+NikolaiLi@users.noreply.github.com>
2025-12-24 15:45:14 +0100

    Follow implementation with small bugs

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -1,24 +1,63 @@
-﻿using Chirp.Infrastructure;
-using Microsoft.AspNetCore.Authorization;   
+﻿using Chirp.Core;
+using Chirp.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Chirp.Web.Pages;
 
-[Authorize]
 public class UserTimelineModel : PageModel
 {
-    private readonly ICheepService _service;
-    public List<CheepViewModel> Cheeps { get; set; } = new();
+    private readonly ICheepRepository _cheepRepo;
+    private readonly IAuthorRepository _authorRepo;
+    
+    public List<CheepViewModel> Cheeps { get; set; } = new List<CheepViewModel>();
+    public bool IsFollowing { get; set; } = false;
 
-    public UserTimelineModel(ICheepService service)
+    public UserTimelineModel(ICheepRepository cheepRepo, IAuthorRepository authorRepo)
     {
-        _service = service;
+        _cheepRepo = cheepRepo;
+        _authorRepo = authorRepo;
     }
 
-    public ActionResult OnGet(string author,[FromQuery] int page = 1)
+    public ActionResult OnGet(string author, [FromQuery] int page = 1)
     {
-        Cheeps = _service.GetCheepsFromAuthor(author, page);
+        var userName = User.Identity?.Name;
+
+        if (User.Identity?.IsAuthenticated == true && userName == author)
+        {
+            Cheeps = _cheepRepo.GetCheepsForTimeline(author, page); 
+        }
+        else 
+        {
+            Cheeps = _cheepRepo.GetCheepsByAuthor(author, page);
+
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                var following = _authorRepo.GetFollowing(userName);
+                IsFollowing = following.Contains(author);
+            }
+        }
+
         return Page();
+    }
+
+    public ActionResult OnPostFollow(string author)
+    {
+        var userName = User.Identity?.Name;
+        if (userName != null) 
+        {
+            _authorRepo.FollowAuthor(userName, author);
+        }
+        return RedirectToPage(new { author = author });
+    }
+
+    public ActionResult OnPostUnfollow(string author)
+    {
+        var userName = User.Identity?.Name;
+        if (userName != null)
+        {
+            _authorRepo.UnfollowAuthor(userName, author);
+        }
+        return RedirectToPage(new { author = author });
     }
 }
