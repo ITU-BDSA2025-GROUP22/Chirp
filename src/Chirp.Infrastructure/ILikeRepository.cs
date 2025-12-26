22dfbf404327d05098e5020af0ec9c75767b1de0
T.H. Engberg <tore0402@gmail.com>
2025-12-26 09:55:31 +0100

    Add more methods for LikeRepository

--- src/Chirp.Infrastructure/ILikeRepository.cs
+++ src/Chirp.Infrastructure/ILikeRepository.cs
@@ -6,4 +6,9 @@ public interface ILikeRepository
 {
     public void AddLike(Like like, int cheepId, int authorId);
     public void RemoveLike(Like like);
+    public Like? GetByAuthorAndCheep(int authorId, int cheepId);
+    public int GetLikeCount(int cheepId);
+    public int GetDislikeCount(int cheepId);
+    public bool HasUserLiked(int authorId, int cheepId);
+    public bool HasUserDisliked(int authorId, int cheepId);
 }
--- src/Chirp.Infrastructure/LikeRepository.cs
+++ src/Chirp.Infrastructure/LikeRepository.cs
@@ -20,15 +20,49 @@ public class LikeRepository : ILikeRepository
         like.AuthorId = authorId;
         _context.Likes.Add(like);
         _context.SaveChanges();
-        
-        var query = _context.Likes
-            .Include(l => l.Cheep)
-            .Where(l => l.Cheep.CheepId == cheepId)
     }
 
     public void RemoveLike(Like like)
     {
         _context.Likes.Remove(like);
         _context.SaveChanges();
+    }
+    
+    /// <summary>
+    /// Method for finding the Like linked to the specified
+    /// authorId and cheepId. Likes are unique, so there can never
+    /// exist two instances of the same like.
+    /// </summary>
+    /// <param name="authorId">authorId</param>
+    /// <param name="cheepId">cheepId</param>
+    /// <returns name="Like">Like</returns>
+    public Like? GetByAuthorAndCheep(int authorId, int cheepId)
+    {
+        return _context.Likes
+            .FirstOrDefault(l => l.AuthorId == authorId && l.CheepId == cheepId);
+    }
+    
+    public int GetLikeCount(int cheepId)
+    {
+        return _context.Likes
+            .Count(l => l.CheepId == cheepId && l.IsLike);
+    }
+    
+    public int GetDislikeCount(int cheepId)
+    {
+        return _context.Likes
+            .Count(l => l.CheepId == cheepId && !l.IsLike);
+    }
+    
+    public bool HasUserLiked(int authorId, int cheepId)
+    {
+        return _context.Likes
+            .Any(l => l.AuthorId == authorId && l.CheepId == cheepId && l.IsLike);
+    }
+    
+    public bool HasUserDisliked(int authorId, int cheepId)
+    {
+        return _context.Likes
+            .Any(l => l.AuthorId == authorId && l.CheepId == cheepId && !l.IsLike);
     }
 }
