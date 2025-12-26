ba3b8714b2af935b53459706d1000d3b63304726
T.H. Engberg <tore0402@gmail.com>
2025-12-26 09:49:58 +0100

    Add LikeRepository.cs and create methods Add and Remove. More to come...

---
+++ src/Chirp.Infrastructure/ILikeRepository.cs
@@ -0,0 +1,9 @@
+﻿using Chirp.Core;
+
+namespace Chirp.Infrastructure;
+
+public interface ILikeRepository
+{
+    public void AddLike(Like like, int cheepId, int authorId);
+    public void RemoveLike(Like like);
+}
---
+++ src/Chirp.Infrastructure/LikeRepository.cs
@@ -0,0 +1,34 @@
+﻿using Chirp.Core;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Chirp.Infrastructure;
+
+public class LikeRepository : ILikeRepository
+{
+    private readonly ChirpContext _context;
+
+    public LikeRepository (ChirpContext context)
+    {
+        _context = context;
+    }
+    
+    public void AddLike(Like like, int cheepId, int authorId)
+    {
+        // Set the foreign keys
+        like.CheepId = cheepId;
+        like.AuthorId = authorId;
+        _context.Likes.Add(like);
+        _context.SaveChanges();
+        
+        var query = _context.Likes
+            .Include(l => l.Cheep)
+            .Where(l => l.Cheep.CheepId == cheepId)
+    }
+
+    public void RemoveLike(Like like)
+    {
+        _context.Likes.Remove(like);
+        _context.SaveChanges();
+    }
+}
